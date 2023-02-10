674b3af7b9225b8fdb4faf94a05df8365a01ee29
Your Name <you@example.com>
2023-02-10 12:05:28 -0500

    primer commit

---
+++ movie/views.py
@@ -0,0 +1,9 @@
+# Create your views here.
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def home(request):
+
+    #return render( request,'home.html')
+    return render(request,'home.html', {'name':'Greg Lim'})
